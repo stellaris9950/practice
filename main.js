4a3ca56fb42dc79364ea2d24834f28de47dbd332
stellaris9950 <andyzihengwei@gmail.com>
2022-11-08 13:28:33 -0700

    2

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
-let cityStr = "Edmonton;Calgary;Vancouver;Saskatoon;Winnipeg";
-let colorArray = cityStr.split(";")
 
 
-.
-alert(colorArray[2]);
+
+let results = ["yes", "no", "maybe"]
+
+
+
+
+function count(){
+    let countYes = 0;
+    let countNo = 0;
+    let countMaybe = 0;
+    for ( let i = 0; i < results.length; i++ ) {
+        if(results[i] = "yes" ){
+          countYes++;
+        }
+        else if(results[i] = "no" ){
+            countNo++;
+          }
+        else {
+            countMaybe++;
+        }
+    }
+
+    alert(countYes)
+    alert(countNo)
+    alert(countMaybe)
+}
+
+
+let age = [18,19,20,21,0,1,2,3]
+
+function ageCount(){
+    let age18 = 0;
+    let age0 = 0;
+    for ( let i = 0; i < age.length; i++ ) {
+        if(age[i] < 18 ){
+          age0++;
+        }
+        else {
+            age18++
+        }
+    }
+    alert(age0);
+    alert(age18);
+}
+
+\
